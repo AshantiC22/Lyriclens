be76d930b4d7fb3d6351652d2936111159faaa07
AshantiC22 <bdkshan18@gmail.com>
2026-08-21 20:36:16 -0500

    Seachbar.jsx first code write

---
+++ lyriclens/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,8 @@
+function SearchBar({ onSearch }) {
+  return (
+    <div>
+      <input placeholder="Enter song name..." />
+    </div>
+  );
+}
+export default SearchBar;
---
+++ lyriclens/src/components/SearchBar/SearchBar.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import SearchBar from "./SearchBar";
+
+describe("SearchBar", () => {
+  test("renders a search input", () => {
+    render(<SearchBar onSearch={() => {}} />);
+    const input = screen.getByPlaceholderText("Enter song name...");
+    expect(input).toBeInTheDocument();
+  });
+});
