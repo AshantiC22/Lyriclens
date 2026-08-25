97dc89ad4131853440f64ca5ffc6a84c811aa7be
ashanti-cocroft <ashanticocroft23@gmail.com>
2026-08-25 18:00:45 -0500

    updated SearchBar.jsx with searchbar function with finding the song and artist

--- lyriclens/src/components/SearchBar/SearchBar.jsx
+++ lyriclens/src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 function SearchBar({ onSearch }) {
+  const [song, setSong] = useState("");
+  const [artist, setArtist] = useState("");
+
+  const handleSubmit = () => {
+    onSearch(song, artist);
+    setSong("");
+    setArtist("");
+  };
+
   return (
     <div>
-      <input placeholder="Enter song name..." />
-      <input placeholder="Enter artist name..." />
-      <button onClick={onSearch}>Search</button>
+      <input
+        value={song}
+        onChange={(e) => setSong(e.target.value)}
+        placeholder="Enter song name..."
+      />
+      <input
+        value={artist}
+        onChange={(e) => setArtist(e.target.value)}
+        placeholder="Enter artist name..."
+      />
+      <button onClick={handleSubmit}>Search</button>
     </div>
   );
 }
